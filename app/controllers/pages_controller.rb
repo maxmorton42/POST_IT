c8047c7291112c4341df2bf708d8bfb6a48e9eb9
Max Morton <max.morton.42@gmail.com>
2019-11-11 20:07:58 -0700

    working branch

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,49 @@
+class PagesController < ApplicationController
+  def index
+    @pages = Page.all
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def new
+    @page = Page.new
+   end
+
+   def create
+    @page = Page.new(pages_params)
+ 
+    if @page.save
+      redirect_to pages_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+  
+    if @page.update(pages_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
+  end
+  
+ 
+  private
+ 
+    def pages_params
+      params.require(:page).permit(:title, :author, :body)
+    end
+end
